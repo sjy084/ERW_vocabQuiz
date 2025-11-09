bd7e7bbd84467c96a15f96d825674c298a2d0c89
sjy084 <lego-shin@sasa.hs.kr>
2025-11-10 03:28:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ function startQuiz() {
   const wordSet = document.getElementById('wordSet').value;
   const modeEnKo = document.getElementById('mode-enko').checked;
   const modeKoEn = document.getElementById('mode-koen').checked;
+  const modeSynonym = document.getElementById('mode-synonym').checked;
 
-  if (!modeEnKo && !modeKoEn) {
+  if (!modeEnKo && !modeKoEn && !modeSynonym) {
     alert('최소 하나의 모드를 선택해주세요!');
     return;
   }
@@ -13,15 +14,12 @@ function startQuiz() {
   // 설정 저장
   localStorage.setItem('wordSet', wordSet);
   
-  let mode;
-  if (modeEnKo && modeKoEn) {
-    mode = 'mixed';
-  } else if (modeEnKo) {
-    mode = 'en-ko';
-  } else {
-    mode = 'ko-en';
-  }
-  localStorage.setItem('mode', mode);
+  let modes = [];
+  if (modeEnKo) modes.push('en-ko');
+  if (modeKoEn) modes.push('ko-en');
+  if (modeSynonym) modes.push('synonym');
+  
+  localStorage.setItem('mode', modes.join(','));
 
   // 퀴즈 페이지로 이동
   location.href = 'quiz.html';
