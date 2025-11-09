fb89c3d0be8d7058f330b0df3088246f7375481a
sjy084 <lego-shin@sasa.hs.kr>
2025-11-10 04:56:19 +0900

    Update words.js

--- words.js
+++ words.js
@@ -19,7 +19,7 @@ const WORD_SETS = {
       { term: "emergency", meaning: ["비상"], synonyms: [] },
       { term: "salient", meaning: ["현저한","중요한"], synonyms: ["prominent"] },
       { term: "saliency", meaning: ["현저성"], synonyms: [] },
-      { term: "indifferent", meaning: ["무관심한"], synonyms: ["unconcerned"] },
+      { term: "indifferent", meaning: ["무관심한"], synonyms: ["unconcerned"] }
     ]
   },
   
@@ -27,7 +27,21 @@ const WORD_SETS = {
   robotdog: {
     name: "Robot Dog",
     words: [
-      { term: "synthetic", meaning: ["합성의", "인조의"], synonyms: ["artificial", "man-made"] }
+      { term: "synthetic", meaning: ["합성의", "인조의"], synonyms: ["artificial", "man-made"] },
+      { term: "bring about", meaning: ["유발하다"], synonyms: ["cause"] },
+      { term: "catastrophe", meaning: ["위기"], synonyms: ["crisis"] },
+      { term: "contend", meaning: ["주장하다"], synonyms: ["argue"] },
+      { term: "mutual", meaning: ["공동의"], synonyms: ["common"] },
+      { term: "vulnerability", meaning: ["취약성"], synonyms: [] },
+      { term: "constitute", meaning: ["구성하다"], synonyms: [] },
+      { term: "seminal", meaning: ["영향력 있는"], synonyms: ["strongly influential"] },
+      { term: "disseminate", meaning: ["생각을 퍼뜨리다"], synonyms: [] },
+      { term: "concede", meaning: ["인정하다"], synonyms: ["admit","agree"] },
+      { term: "emergence", meaning: ["등장","창발"], synonyms: [] },
+      { term: "emergency", meaning: ["비상"], synonyms: [] },
+      { term: "salient", meaning: ["현저한","중요한"], synonyms: ["prominent"] },
+      { term: "saliency", meaning: ["현저성"], synonyms: [] },
+      { term: "indifferent", meaning: ["무관심한"], synonyms: ["unconcerned"] }
     ]
   }
 };
